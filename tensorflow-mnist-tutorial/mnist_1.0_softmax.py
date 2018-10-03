7fd8b0865bfd29f25e0ae83af15c87e0baf01fc4
jeff <g294631795>
2018-10-03 17:11:43 +0800

    mnist初步翻译完

--- tensorflow-mnist-tutorial/mnist_1.0_softmax.py
+++ tensorflow-mnist-tutorial/mnist_1.0_softmax.py
@@ -55,47 +55,47 @@ b = tf.Variable(tf.zeros([10]))
 # -1 在形状定义中表示“保留元素数量的唯一可能纬度”。实践中，它将是最小批量的图像数。
 XX = tf.reshape(X, [-1, 784])
 
-# The model
+# 模型
 Y = tf.nn.softmax(tf.matmul(XX, W) + b)
 
-# loss function: cross-entropy = - sum( Y_i * log(Yi) )
-#                           Y: the computed output vector
-#                           Y_: the desired output vector
+# 损失函数: cross-entropy = - sum( Y_i * log(Yi) )
+#                           Y:计算后输出向量
+#                           Y_：所需要输出的向量
 
-# cross-entropy
-# log takes the log of each element, * multiplies the tensors element by element
-# reduce_mean will add all the components in the tensor
-# so here we end up with the total cross-entropy for all images in the batch
-cross_entropy = -tf.reduce_mean(Y_ * tf.log(Y)) * 1000.0  # normalized for batches of 100 images,
-                                                          # *10 because  "mean" included an unwanted division by 10
+# 交叉熵
+# 以log获取每个元素的log，以元素乘以张量元素
+# reduce_mean（减少平均值）将添加张量中的所有组件
+# 所以这里我们得到在批量中所有图片的总交叉熵
+cross_entropy = -tf.reduce_mean(Y_ * tf.log(Y)) * 1000.0  # 一批一百张图的标准化
+                                                          # 乘10因为平均值包含一个不需要的除以10
 
-# accuracy of the trained model, between 0 (worst) and 1 (best)
-correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# 训练模型的精确度介于0（坏）和1（好）之间
+correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1)) #正确预测
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) #精确度
 
-# training, learning rate = 0.005
+# 训练中，学习速率=0.005
 train_step = tf.train.GradientDescentOptimizer(0.005).minimize(cross_entropy)
 
-# matplotlib visualisation
+# matplotlib可视化
 allweights = tf.reshape(W, [-1])
 allbiases = tf.reshape(b, [-1])
 I = tensorflowvisu.tf_format_mnist_images(X, Y, Y_)  # assembles 10x10 images by default
 It = tensorflowvisu.tf_format_mnist_images(X, Y, Y_, 1000, lines=25)  # 1000 images on 25 lines
 datavis = tensorflowvisu.MnistDataVis()
 
-# init
+# 初始化
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
 
-# You can call this function in a loop to train the model, 100 images at a time
+# 你可以在循环中调用此函数以训练模型，一次一百张图
 def training_step(i, update_test_data, update_train_data):
 
-    # training on batches of 100 images with 100 labels
+    # 对一百张图带一百个标签的批次进行训练
     batch_X, batch_Y = mnist.train.next_batch(100)
 
-    # compute training values for visualisation
+    # 计算可视化的训练值
     if update_train_data:
         a, c, im, w, b = sess.run([accuracy, cross_entropy, I, allweights, allbiases], feed_dict={X: batch_X, Y_: batch_Y})
         datavis.append_training_curves_data(i, a, c)
@@ -104,22 +104,24 @@ def training_step(i, update_test_data, update_train_data):
         print(str(i) + ": accuracy:" + str(a) + " loss: " + str(c))
 
     # compute test values for visualisation
+    # 计算可视化的测试值
     if update_test_data:
         a, c, im = sess.run([accuracy, cross_entropy, It], feed_dict={X: mnist.test.images, Y_: mnist.test.labels})
         datavis.append_test_curves_data(i, a, c)
         datavis.update_image2(im)
         print(str(i) + ": ********* epoch " + str(i*100//mnist.train.images.shape[0]+1) + " ********* test accuracy:" + str(a) + " test loss: " + str(c))
 
-    # the backpropagation training step
+    # 反向传播训练步骤
     sess.run(train_step, feed_dict={X: batch_X, Y_: batch_Y})
 
 
 datavis.animate(training_step, iterations=2000+1, train_data_update_freq=10, test_data_update_freq=50, more_tests_at_start=True)
 
-# to save the animation as a movie, add save_movie=True as an argument to datavis.animate
-# to disable the visualisation use the following line instead of the datavis.animate line
+# 将动画保存为影片，将save_movie=True添加为datavis.animate的参数。
+# 要禁止可视化使用以下行，而不是datavis.animate行。
 # for i in range(2000+1): training_step(i, i % 50 == 0, i % 10 == 0)
 
 print("max test accuracy: " + str(datavis.get_max_test_accuracy()))
 
 # final max test accuracy = 0.9268 (10K iterations). Accuracy should peak above 0.92 in the first 2000 iterations.
+# 最终最大测试精度=0.9268（10k次迭代）。在前两千次迭代中精确度达到0.92以上。
