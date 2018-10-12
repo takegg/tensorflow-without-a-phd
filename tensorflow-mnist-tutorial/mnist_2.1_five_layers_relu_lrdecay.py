92e3bfd1550b672d3f1066d44b35aa63d235db1c
gaojianfengwb <294631795@qq.com>
2018-10-12 13:10:24 +0800

    all

--- tensorflow-mnist-tutorial/mnist_2.1_five_layers_relu_lrdecay.py
+++ tensorflow-mnist-tutorial/mnist_2.1_five_layers_relu_lrdecay.py
@@ -51,8 +51,8 @@ L = 200
 M = 100
 N = 60
 O = 30
-# Weights initialised with small random values between -0.2 and +0.2
-# When using RELUs, make sure biases are initialised with small *positive* values for example 0.1 = tf.ones([K])/10
+# 权重初始化为-0.2到+0.2之间的随机小值
+# 当使用RELUs时，确保biases初始化为小*正*值，例如0.1 = tf.ones([K])/10
 W1 = tf.Variable(tf.truncated_normal([784, L], stddev=0.1))  # 784 = 28 * 28
 B1 = tf.Variable(tf.ones([L])/10)
 W2 = tf.Variable(tf.truncated_normal([L, M], stddev=0.1))
@@ -73,17 +73,17 @@ Y4 = tf.nn.relu(tf.matmul(Y3, W4) + B4)
 Ylogits = tf.matmul(Y4, W5) + B5
 Y = tf.nn.softmax(Ylogits)
 
-# cross-entropy loss function (= -sum(Y_i * log(Yi)) ), normalised for batches of 100  images
-# TensorFlow provides the softmax_cross_entropy_with_logits function to avoid numerical stability
-# problems with log(0) which is NaN
+# 交叉熵损失函数(= -sum(Y_i * log(Yi)) )，标准化为100个图片的批量。
+# TensorFlow提供softmax_cross_entropy_with_logits函数以避免数值稳定性。
+# NaN的问题用log(0)
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Ylogits, labels=Y_)
 cross_entropy = tf.reduce_mean(cross_entropy)*100
 
-# accuracy of the trained model, between 0 (worst) and 1 (best)
+# 训练模型的精确度在0(最坏)和1(最好)之间
 correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-# matplotlib visualisation
+# matplotlib可视化
 allweights = tf.concat([tf.reshape(W1, [-1]), tf.reshape(W2, [-1]), tf.reshape(W3, [-1]), tf.reshape(W4, [-1]), tf.reshape(W5, [-1])], 0)
 allbiases  = tf.concat([tf.reshape(B1, [-1]), tf.reshape(B2, [-1]), tf.reshape(B3, [-1]), tf.reshape(B4, [-1]), tf.reshape(B5, [-1])], 0)
 I = tensorflowvisu.tf_format_mnist_images(X, Y, Y_)
@@ -91,23 +91,23 @@ It = tensorflowvisu.tf_format_mnist_images(X, Y, Y_, 1000, lines=25)
 datavis = tensorflowvisu.MnistDataVis()
 
 # training step
-# the learning rate is: # 0.0001 + 0.003 * (1/e)^(step/2000)), i.e. exponential decay from 0.003->0.0001
+# 学习速率是：0.0001 + 0.003 * (1/e)^(step/2000))，即指数衰减从0.003到0.0001
 lr = 0.0001 +  tf.train.exponential_decay(0.003, step, 2000, 1/math.e)
 train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
 
-# init
+# 初始化
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
 
-# You can call this function in a loop to train the model, 100 images at a time
+# 你可以在训练模型的循环中调用此函数，每次100张图
 def training_step(i, update_test_data, update_train_data):
 
-    # training on batches of 100 images with 100 labels
+    # 在100张带100个标签的图的批上训练
     batch_X, batch_Y = mnist.train.next_batch(100)
 
-    # compute training values for visualisation
+    # 计算可视化的训练值
     if update_train_data:
         a, c, im, w, b, l = sess.run([accuracy, cross_entropy, I, allweights, allbiases, lr],
                                      feed_dict={X: batch_X, Y_: batch_Y, step: i})
@@ -116,35 +116,35 @@ def training_step(i, update_test_data, update_train_data):
         datavis.update_image1(im)
         datavis.append_data_histograms(i, w, b)
 
-    # compute test values for visualisation
+    # 计算可视化的测试值
     if update_test_data:
         a, c, im = sess.run([accuracy, cross_entropy, It], feed_dict={X: mnist.test.images, Y_: mnist.test.labels})
         print(str(i) + ": ********* epoch " + str(i*100//mnist.train.images.shape[0]+1) + " ********* test accuracy:" + str(a) + " test loss: " + str(c))
         datavis.append_test_curves_data(i, a, c)
         datavis.update_image2(im)
 
-    # the backpropagation training step
+    # 反向传播training step
     sess.run(train_step, feed_dict={X: batch_X, Y_: batch_Y, step: i})
 
 datavis.animate(training_step, iterations=10000+1, train_data_update_freq=20, test_data_update_freq=100, more_tests_at_start=True)
 
-# to save the animation as a movie, add save_movie=True as an argument to datavis.animate
-# to disable the visualisation use the following line instead of the datavis.animate line
+# 保存动画为视频，添加save_movie=True作为datavis.animate的一个参数
+# 使用以下行代替datavis.animate行以禁止可视化
 # for i in range(10000+1): training_step(i, i % 100 == 0, i % 20 == 0)
 
 print("max test accuracy: " + str(datavis.get_max_test_accuracy()))
 
-# Some results to expect:
-# (In all runs, if sigmoids are used, all biases are initialised at 0, if RELUs are used,
-# all biases are initialised at 0.1 apart from the last one which is initialised at 0.)
+# 一些预期结果：
+# (在运行中，如果使用sigmoids，所有biasess初始化为0，如果使用RELUs，除去初始化为0的最后一个所有biases初始化为0.1)
 
-## learning rate = 0.003, 10K iterations
-# final test accuracy = 0.9788 (sigmoid - slow start, training cross-entropy not stabilised in the end)
-# final test accuracy = 0.9825 (relu - above 0.97 in the first 1500 iterations but noisy curves)
+## 学习速率为0.003,10k的迭代
+# 最终测试准确率为0.9788(sigmoid-缓慢开始，训练cross-entropy在结束时不稳定)
+# 最终测试准确率为0.9825（relu-在第一次1500个迭代中在0.97以上，但曲线嘈杂）
 
-## now with learning rate = 0.0001, 10K iterations
-# final test accuracy = 0.9722 (relu - slow but smooth curve, would have gone higher in 20K iterations)
+## 现在已0.0001的学习速率，10k的迭代
+# 最终测试准确率为0.9722(relu-慢但是曲线平滑，在20k的迭代中将更高)
 
-## decaying learning rate from 0.003 to 0.0001 decay_speed 2000, 10K iterations
-# final test accuracy = 0.9746 (sigmoid - training cross-entropy not stabilised)
+## 学习速率衰减从0.003到0.0001，衰减速率为2000,10k迭代。
+# 最终测试准确率为0.9746(sigmoid-训练交叉熵不稳定)
 # final test accuracy = 0.9824 (relu - training set fully learned, test accuracy stable)
+# 最终测试准确率为0.9824(relu-训练集充分学习，测试集准确率稳定)
